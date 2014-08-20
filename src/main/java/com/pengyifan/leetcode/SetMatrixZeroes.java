d6897b6ace66868779e4541c4e3bc35b5436c953
Yifan Peng <yfpeng@google.com>
2014-08-20 14:32:06 -0700

    set matrix zeroes

--- src/main/java/com/pengyifan/leetcode/SetMatrixZeroes.java
+++ src/main/java/com/pengyifan/leetcode/SetMatrixZeroes.java
@@ -16,27 +16,80 @@ package com.pengyifan.leetcode;
  */
 public class SetMatrixZeroes {
 
+  /**
+   * O(m + n) space
+   */
   public void setZeroes(int[][] matrix) {
     if (matrix == null || matrix.length == 0) {
       return;
     }
 
-    int row[] = new int[matrix.length];
-    int col[] = new int[matrix[0].length];
+    boolean row[] = new boolean[matrix.length];
+    boolean col[] = new boolean[matrix[0].length];
 
     for (int i = 0; i < matrix.length; i++) {
       for (int j = 0; j < matrix[i].length; j++) {
         if (matrix[i][j] == 0) {
-          row[i] = -1;
-          col[j] = -1;
+          row[i] = true;
+          col[j] = true;
         }
       }
     }
     for (int i = 0; i < matrix.length; i++) {
       for (int j = 0; j < matrix[i].length; j++) {
-        if (row[i] == -1 || col[j] == -1) {
+        if (row[i] || col[j]) {
           matrix[i][j] = 0;
         }
+      }
+    }
+  }
+
+  /**
+   * a constant space solution
+   */
+  public void setZeroes2(int[][] matrix) {
+    if (matrix == null || matrix.length == 0) {
+      return;
+    }
+
+    boolean rowHasZero = false;
+    boolean colHasZero = false;
+    // check first row
+    for (int i = 0; i < matrix[0].length; i++) {
+      if (matrix[0][i] == 0) {
+        rowHasZero = true;
+      }
+    }
+    // check first column
+    for (int i = 0; i < matrix.length; i++) {
+      if (matrix[i][0] == 0) {
+        colHasZero = true;
+      }
+    }
+
+    for (int i = 1; i < matrix.length; i++) {
+      for (int j = 1; j < matrix[i].length; j++) {
+        if (matrix[i][j] == 0) {
+          matrix[0][j] = 0;
+          matrix[i][0] = 0;
+        }
+      }
+    }
+    for (int i = 1; i < matrix.length; i++) {
+      for (int j = 1; j < matrix[i].length; j++) {
+        if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+          matrix[i][j] = 0;
+        }
+      }
+    }
+    if (rowHasZero) {
+      for (int i = 0; i < matrix[0].length; i++) {
+        matrix[0][i] = 0;
+      }
+    }
+    if (colHasZero) {
+      for (int i = 0; i < matrix.length; i++) {
+        matrix[i][0] = 0;
       }
     }
   }
