b4eab5465712a4f055d58085560a6e7ed70823d7
Yifan Peng <ypeng@cis.udel.edu>
2014-09-24 13:05:03 -0400

    generate parentheses

---
+++ src/main/java/com/pengyifan/leetcode/GenerateParentheses.java
@@ -0,0 +1,13 @@
+package com.pengyifan.leetcode;
+
+import java.util.Collections;
+import java.util.List;
+
+
+public class GenerateParentheses {
+  public List<String> generateParenthesis(int n) {
+    if (n == 0) {
+      return Collections.emptyList();
+    }
+  }
+}
