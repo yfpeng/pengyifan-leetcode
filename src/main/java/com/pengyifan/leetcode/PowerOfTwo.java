dd7e437b37a6562e37e258d749ff349bb3bcd8ec
Yifan Peng <yfpeng@udel.edu>
2016-06-17 17:20:13 -0400

    Power of Two

---
+++ src/main/java/com/pengyifan/leetcode/PowerOfTwo.java
@@ -0,0 +1,19 @@
+package com.pengyifan.leetcode;
+
+/**
+ * Given an integer, write a function to determine if it is a power of two.
+ */
+public class PowerOfTwo {
+  public boolean isPowerOfTwo(int n) {
+    if (n <= 0) {
+      return false;
+    }
+    while (n != 1) {
+      if (n %2 == 1) {
+        return  false;
+      }
+      n /= 2;
+    }
+    return true;
+  }
+}
