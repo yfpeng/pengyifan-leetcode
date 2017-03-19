b93f67378b385791ffa0b2a79f751c15335d5fa5
Yifan Peng <yfpeng@udel.edu>
2017-03-19 00:10:33 -0400

    number complement

--- src/main/python/pyleetcode/hamming_distance.py
+++ src/main/python/pyleetcode/hamming_distance.py
@@ -4,15 +4,16 @@ The Hamming distance between two integers is the number of positions at which th
 Given two integers x and y, calculate the Hamming distance.
 
 Note:
-0 <= x, y < 231.
+    0 <= x, y < 231.
 
 Example:
 
-Input: x = 1, y = 4
+    Input: x = 1, y = 4
 
-Output: 2
+    Output: 2
 
 Explanation:
+
 1   (0 0 0 1)
 4   (0 1 0 0)
        ^   ^
